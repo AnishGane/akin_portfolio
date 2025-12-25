bfca46f4cd96521ffcbfe8c85525631e15b377a1
Anish Gane <anishgane10@gmail.com>
2025-12-25 19:12:57 +0545

    Add smooth scroll functionality to LandingSection

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,6 +1,11 @@
 import { motion } from "motion/react";
 
 const LandingSection = ({ showContent }: { showContent: boolean }) => {
+  const scrollToAbout = () => {
+    const aboutSection = document.getElementById("about");
+    aboutSection?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="relative">
       <motion.div
@@ -32,26 +37,25 @@ const LandingSection = ({ showContent }: { showContent: boolean }) => {
           duration: 1.1,
           ease: [0.22, 1, 0.36, 1], // easeOutExpo-like
         }}
-        className="absolute bottom-15 tracking-wide font-medium  text-gray-600/70 left-1/2 -translate-x-1/2"
+        onClick={scrollToAbout}
+        className="absolute bottom-15 tracking-wide font-medium flex flex-col gap-1 items-center cursor-pointer  text-gray-600/70 left-1/2 -translate-x-1/2"
       >
-        <a href="#about" className="flex items-center flex-col justify-center gap-2">
-          <p className="uppercase">Scroll</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon icon-tabler animate-bounce icons-tabler-outline icon-tabler-chevron-down"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M6 9l6 6l6 -6" />
-          </svg>
-        </a>
+        <p className="uppercase">Scroll</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="icon icon-tabler animate-bounce icons-tabler-outline icon-tabler-chevron-down"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M6 9l6 6l6 -6" />
+        </svg>
       </motion.div>
     </div>
   );
